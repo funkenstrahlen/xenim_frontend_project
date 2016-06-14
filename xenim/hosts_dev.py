8db5abe4b0267efc67e9756acd332e3c234969f5
Robert Weidlich <github@robertweidlich.de>
2016-06-14 08:49:42 +0200

    Fixes

--- xenim/hosts_dev.py
+++ xenim/hosts_dev.py
@@ -4,6 +4,6 @@ host_patterns = patterns('',
     host(r'127.0.0.1:8002', 'xenim.urls.feeds', name="feeds", scheme="http://"),
     host(r'127.0.0.1:8003', 'xenim.urls.review', name="review", scheme="http://"),
     host(r'127.0.0.1:8001', 'xenim.urls.dashboard', name="dashboard", scheme="http://"),
-    host(r'127.0.0.1:8000', 'xenim.urls.www', name="www", scheme="http://"),
-    host(r'127.0.0.1:8004', 'xenim.urls.shorturl', name="shorturl", scheme="http://"),
+    host(r'127.0.0.1:8000', 'xenim.urls.www', name="www"),
+    host(r'127.0.0.1:8004', 'xenim.urls.shorturl', name="shorturl"),
 )
